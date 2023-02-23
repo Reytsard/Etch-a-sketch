61e9865cca5772aaead254cd28a0078fdcb7573f
Rey <ronaldreyjr061201@gmail.com>
2023-02-23 08:36:54 +0800

    added ColorChange and NewArea Button

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-function changeColor(e){
-    
-}
+var sideSize = 16;
 
 let playArea = document.querySelector('.playArea');
 
-for(let i = 1 ; i <= 16; i++){
+for(let i = 1 ; i <= sideSize; i++){
     playArea.appendChild(document.createElement('div')).setAttribute('id','row');
 
 }
 let row = playArea.querySelectorAll("div");
 
 row.forEach(e => {
-    for(let i = 1; i <= 16; i++){
+    for(let i = 1; i <= sideSize; i++){
         e.appendChild(document.createElement('div')).setAttribute('id','col');
     }
 });
@@ -23,4 +21,11 @@ col.forEach((e) => {
     }
 })
 
+let rstrtButton = document.querySelector('#rstrtBtn');
+rstrtButton.onclick = () => { 
+    // sideSize = Number(prompt("What is the new size?").valueOf);
+    let size = prompt("Whats the new SIZE?");
+    sideSize = Number.parseInt(size);
+    console.log(size);
+}
 
