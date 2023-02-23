e607bda69ab83b673e9cb9d86197a7b994ee9117
Rey <ronaldreyjr061201@gmail.com>
2023-02-23 14:06:54 +0800

    added JS to adjust screen height and width

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-var sideSize = 16;
+//functions
+function makePlayArea(sideSize){
 
+    //makes the row
+    for(let i = 1 ; i <= sideSize; i++){
+        playArea.appendChild(document.createElement('div')).setAttribute('id','row');
+    }
+    let row = playArea.querySelectorAll("div");
+    row.forEach(e => {
+        for(let i = 1; i <= sideSize; i++){
+            e.appendChild(document.createElement('div')).setAttribute('id','col');
+            e.style.height =  `calc(100% / ${sideSize})`;            
+        }
+    });
+    
+
+    let col = playArea.querySelectorAll('#col');
+        col.forEach((e) => {
+            e.style.width =  `calc(100% / ${sideSize})`;            
+            e.onmouseover = () => {
+                e.style['background-color'] = 'white'
+            }
+        })
+    }
+
+//main
+var sideSize = 16;
 let playArea = document.querySelector('.playArea');
 
-for(let i = 1 ; i <= sideSize; i++){
-    playArea.appendChild(document.createElement('div')).setAttribute('id','row');
-
-}
-let row = playArea.querySelectorAll("div");
-
-row.forEach(e => {
-    for(let i = 1; i <= sideSize; i++){
-        e.appendChild(document.createElement('div')).setAttribute('id','col');
-    }
-});
-
-let col = playArea.querySelectorAll('#col');
-col.forEach((e) => {
-    e.onmouseover = () => {
-        e.style['background-color'] = 'white'
-    }
-})
+makePlayArea(sideSize);
 
 let rstrtButton = document.querySelector('#rstrtBtn');
 rstrtButton.onclick = () => { 
-    // sideSize = Number(prompt("What is the new size?").valueOf);
-    let size = prompt("Whats the new SIZE?");
+    let size = prompt("Whats the new SIZE? (1-100)");
     sideSize = Number.parseInt(size);
-    console.log(size);
-    updatePlayArea(sideSize);
+    while(sideSize < 1 || sideSize > 100){
+        size = prompt("Whats the new SIZE? (1-100)");
+    }
+        playArea.replaceChildren("");
+        makePlayArea(sideSize);
+        
 }
 
-function updatePlayArea(sideSize){
-    
-}
